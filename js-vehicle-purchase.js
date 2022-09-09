073e6b71aff8dab22e90a903c7a506f90251e07e
jeffspatafora <jeffspatafora@gmail.com>
2022-09-09 16:02:12 -0400

    adds problem work so far

---
+++ js-vehicle-purchase.js
@@ -0,0 +1,46 @@
+// // console.log('*' > '&');
+
+// export function needsLicense(kind) {
+//   // if (kind === 'car') {
+//   //   return true;
+//   // } else if (kind === 'truck') {
+//   //   return true;
+//   // } else {
+//   //   return false;
+//   // };
+
+//   // if (kind === 'car' || kind === 'truck') {
+//   //   return true;
+//   // } else {
+//   //   return false;
+//   // }
+//   return kind.toLowerCase() === 'car' || kind === 'truck';
+// }
+
+// /**
+//  * Helps choosing between two options by recommending the one that
+//  * comes first in dictionary order.
+//  *
+//  * @param {string} option1
+//  * @param {string} option2
+//  * @returns {string} a sentence of advice which option to choose
+//  */
+// export function chooseVehicle(option1, option2) {
+//   if (option1 > option2) {
+//     return `${option2} is clearly the better choice.`;
+//   } else {
+//     return `${option1} is clearly the better choice.`;
+//   };
+// }
+
+// /**
+//  * Calculates an estimate for the price of a used vehicle in the dealership
+//  * based on the original price and the age of the vehicle.
+//  *
+//  * @param {number} originalPrice
+//  * @param {number} age
+//  * @returns {number} expected resell price in the dealership
+//  */
+// export function calculateResellPrice(originalPrice, age) {
+//   throw new Error('Please implement the calculateResellPrice function');
+// }
