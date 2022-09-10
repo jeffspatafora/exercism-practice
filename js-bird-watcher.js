32881e2fb620792894995a7ef7fd8447d8da8d3f
jeffspatafora <jeffspatafora@gmail.com>
2022-09-10 14:20:51 -0400

    adds starting work

---
+++ js-bird-watcher.js
@@ -0,0 +1,29 @@
+// export function totalBirdCount(birdsPerDay) {
+//   let totalBirds = 0;
+//   for (let i = 0; i < birdsPerDay.length; i++) {
+//     totalBirds = totalBirds + birdsPerDay[i];
+//   }
+//   return totalBirds;
+// }
+
+// /**
+//  * Calculates the total number of birds seen in a specific week.
+//  *
+//  * @param {number[]} birdsPerDay
+//  * @param {number} week
+//  * @returns {number} birds counted in the given week
+//  */
+// export function birdsInWeek(birdsPerDay, week) {
+//   ;
+// }
+
+// /**
+//  * Fixes the counting mistake by increasing the bird count
+//  * by one for every second day.
+//  *
+//  * @param {number[]} birdsPerDay
+//  * @returns {number[]} corrected bird count data
+//  */
+// export function fixBirdCountLog(birdsPerDay) {
+//   throw new Error('Please implement the fixBirdCountLog function');
+// }
