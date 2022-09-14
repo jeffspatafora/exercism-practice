538ce646644f756aeac820b8678569d4bd4a6c94
jeffspatafora <jeffspatafora@gmail.com>
2022-09-14 18:03:41 -0400

    adds problem 3 work and solutions, one refactored to not create new array

--- js-bird-watcher.js
+++ js-bird-watcher.js
@@ -13,31 +13,31 @@
 //  * @param {number} week
 //  * @returns {number} birds counted in the given week
 //  */
-function birdsInWeek(birdsPerDay, week) {
-  let totalBirdsInWeek = 0;
-  let weekStart = (7 * week) - 7;
-  for (let i = 0; i < 7; i++) {
-    totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[weekStart];
-    weekStart++;
-  }
-  return console.log(totalBirdsInWeek);
-  // let totalBirdsInWeek = 0;
-  // for (let i = (7 * week) - 7; i < birdsPerDay.length - ((7 * week) - 7); i++) {
-  //   console.log(i);
-  //   totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
-  // }
-  // return console.log(totalBirdsInWeek);
-  // let totalBirdsInWeek = 0;
-  // for (let i = (7 * week) - 7; i <= 7; i++) {
-  //   console.log(i);
-  //   totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
-  // }
-  // return console.log(totalBirdsInWeek);
-}
+// function birdsInWeek(birdsPerDay, week) {
+//   let totalBirdsInWeek = 0;
+//   let weekStart = (7 * week) - 7;
+//   for (let i = 0; i < 7; i++) {
+//     totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[weekStart];
+//     weekStart++;
+//   }
+//   return console.log(totalBirdsInWeek);
+//   // let totalBirdsInWeek = 0;
+//   // for (let i = (7 * week) - 7; i < birdsPerDay.length - ((7 * week) - 7); i++) {
+//   //   console.log(i);
+//   //   totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
+//   // }
+//   // return console.log(totalBirdsInWeek);
+//   // let totalBirdsInWeek = 0;
+//   // for (let i = (7 * week) - 7; i <= 7; i++) {
+//   //   console.log(i);
+//   //   totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
+//   // }
+//   // return console.log(totalBirdsInWeek);
+// }
 
-birdsInWeek([4, 7, 3, 2, 1, 1, 2, 0, 2, 3, 2, 7, 1, 3, 0, 6, 5, 3, 7, 2, 3], 2);
-birdsInWeek([3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0, 8, 0], 1);
-birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2);
+// birdsInWeek([4, 7, 3, 2, 1, 1, 2, 0, 2, 3, 2, 7, 1, 3, 0, 6, 5, 3, 7, 2, 3], 2);
+// birdsInWeek([3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0, 8, 0], 1);
+// birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2);
 
 // /**
 //  * Fixes the counting mistake by increasing the bird count
@@ -46,6 +46,24 @@ birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2);
 //  * @param {number[]} birdsPerDay
 //  * @returns {number[]} corrected bird count data
 //  */
-// export function fixBirdCountLog(birdsPerDay) {
-//   throw new Error('Please implement the fixBirdCountLog function');
+// function fixBirdCountLog(birdsPerDay) {
+//   const correctedCount = [];
+//   for (let i = 0; i < birdsPerDay.length; i += 2) {
+//     correctedCount.push(birdsPerDay[i] + 1);
+//     correctedCount.push(birdsPerDay[i + 1]);
+//   }
+//   return console.log(correctedCount);
 // }
+
+// fixBirdCountLog([2, 5, 0, 7, 4, 1]);
+
+function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i += 2) {
+    birdsPerDay.splice(i, 1, birdsPerDay[i] + 1);
+  }
+  return console.log(birdsPerDay);
+}
+
+fixBirdCountLog([2, 5, 0, 7, 4, 1]);
+
+// [3, 5, 1, 7, 5, 1]
