528d92c6dc5b39f0ef1f1a13ead3669f26cf9cc8
jeffspatafora <jeffspatafora@gmail.com>
2022-09-14 12:56:52 -0400

    adds still working on solution

--- js-bird-watcher.js
+++ js-bird-watcher.js
@@ -15,11 +15,18 @@
 //  */
 function birdsInWeek(birdsPerDay, week) {
   let totalBirdsInWeek = 0;
-  for (let i = (7 * week) - 7; i < birdsPerDay.length - ((7 * week) - 7); i++) {
-    console.log(i);
-    totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
+  let weekStart = (7 * week) - 7;
+  for (let i = 0; i <= 7; i++) {
+    totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[weekStart];
+    weekStart++;
   }
   return console.log(totalBirdsInWeek);
+  // let totalBirdsInWeek = 0;
+  // for (let i = (7 * week) - 7; i < birdsPerDay.length - ((7 * week) - 7); i++) {
+  //   console.log(i);
+  //   totalBirdsInWeek = totalBirdsInWeek + birdsPerDay[i];
+  // }
+  // return console.log(totalBirdsInWeek);
   // let totalBirdsInWeek = 0;
   // for (let i = (7 * week) - 7; i <= 7; i++) {
   //   console.log(i);
@@ -29,8 +36,8 @@ function birdsInWeek(birdsPerDay, week) {
 }
 
 birdsInWeek([4, 7, 3, 2, 1, 1, 2, 0, 2, 3, 2, 7, 1, 3, 0, 6, 5, 3, 7, 2, 3], 2);
-// birdsInWeek([3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0, 8, 0], 1);
-// birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2);
+birdsInWeek([3, 0, 5, 1, 0, 4, 1, 0, 3, 4, 3, 0, 8, 0], 1);
+birdsInWeek([2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1], 2);
 
 // /**
 //  * Fixes the counting mistake by increasing the bird count
